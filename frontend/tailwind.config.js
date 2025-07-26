d2537d55edb3a307ec7d531e30f851cfe60bfcb2
florent <florentsahiti06@gmail.com>
2025-07-26 16:24:09 +0200

    Navbar Creation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary': '#5f6FFF'
+      }
+    },
   },
   plugins: [],
 }
