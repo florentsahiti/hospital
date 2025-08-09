c122f3ae750271e87beaca676e65adb085376d40
florentsahiti <florentsahiti06@gmail.com>
2025-08-09 21:51:15 +0200

    test

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
+  theme: {
+    extend: {
+      colors:{
+        'primary': '#5f6FFF'
+      }
+    },
+  },
+  plugins: [],
+}
+
