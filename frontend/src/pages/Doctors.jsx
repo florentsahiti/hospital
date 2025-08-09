86180cc736b6f89a972837537f9d19234d89d9f7
florentsahiti <florentsahiti06@gmail.com>
2025-08-09 21:58:07 +0200

    Error Fix

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 const Doctors = () => {
   return (
     <div>
-<<<<<<< HEAD
       <h1>Doctors</h1>
-=======
-      
->>>>>>> 2ecfd0dd7be96d39905fc8a362caf6dba03a9d65
     </div>
   )
 }
