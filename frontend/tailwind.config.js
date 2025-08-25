529dc18078f507d36b9d8e96129f5f618b83d3aa
florentsahiti <florentsahiti06@gmail.com>
2025-08-25 16:27:17 +0200

    TopDoctors page and context of frontend project

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,10 @@ export default {
     extend: {
       colors:{
         'primary': '#5f6FFF'
-      }
+      },
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fill, minmax(200px, 1fr))',
+      },
     },
   },
   plugins: [],
